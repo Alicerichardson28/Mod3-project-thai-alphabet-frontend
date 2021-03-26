a820d8056a401c0bec130cd1246d206f7ec0a698
Parada <paradafaii28@gmail.com>
2021-03-26 15:40:10 -0600

    update url

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const linkImage = document.querySelector('.imageBackground')
 
 // const imageclick = document.createElement('image')
 linkImage.addEventListener('click', ()=> {
-    window.location.href = "https://thai-alphabet-flashcard.web.app/";
+    window.location.href = "https://thai-alphabet-flashcard.web.app/mainpage.js";
 })
